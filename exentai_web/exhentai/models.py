fef84ff44f068962b8647f6f98b603824a3bf9a8
mdlovewho@163.com <ma199141>
2016-09-10 01:17:17 +0800

    Gallery数据库

--- exentai_web/exhentai/models.py
+++ exentai_web/exhentai/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class ExGallery(models.Model):
+    """
+    用于存储的画集实体类
+    """
+    id = models.IntegerField(primary_key=True)
+    root_path = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    name_n = models.CharField(max_length=255)
+    name_j = models.CharField(max_length=255)
+    type = models.IntegerField()
+    language = models.IntegerField()
+    translator = models.CharField(max_length=255)
+    pages = models.IntegerField()
+    length = models.IntegerField()
+    posted = models.DateTimeField()
+    is_anthology = models.IntegerField()
+    rating = models.IntegerField()
+    status = models.IntegerField()
+    last_view = models.DateTimeField()
+
